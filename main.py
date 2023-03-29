12e3ef7457231d25c213fc3aa278791298b016e5
Lecole Cole <lecole@users.noreply.github.com>
2023-03-28 18:52:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -929,6 +929,6 @@ if __name__ == "__main__":
             dst = os.path.join(dst, "debug_runs", name)
             os.makedirs(os.path.split(dst)[0], exist_ok=True)
             os.rename(logdir, dst)
-        if trainer.global_rank == 0:
-            print("🔹Training complete🔹. max_training_steps reached or we blew up.")
+        # if trainer.global_rank == 0:
+            # print("🔹Training complete🔹. max_training_steps reached or we blew up.")
             # print(trainer.profiler.summary())
